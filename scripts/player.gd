f8393a442de286b17dc4bc11f2e89f514b35d635
Nikola Talevski <talevskinikola1409@gmail.com>
2025-02-06 20:17:20 +0100

    Add jumping

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,10 +5,11 @@ extends CharacterBody3D
 @onready var anim_player = $Pivot/StylizedCharacter/AnimationPlayer
 
 var target_velocity = Vector3.ZERO
+@export var jump_impulse = 30
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
-
+	
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
 		anim_player.play("Armature|Run")
@@ -22,18 +23,26 @@ func _physics_process(delta):
 		direction.z -= 1
 		anim_player.play("Armature|Run")
 	
-	if Input.is_action_just_released("move_right") or Input.is_action_just_released("move_left") or Input.is_action_just_released("move_forward") or Input.is_action_just_released("move_back"):
-		anim_player.play("Armature|Iddle")
-
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.basis = Basis.looking_at(direction)
-
+	
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
-
+	
 	if not is_on_floor():
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-
+	
 	velocity = target_velocity
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
+		anim_player.play("Armature|Jump")
+		
+	if (Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right") or Input.is_action_pressed("move_forward") or Input.is_action_pressed("move_back")) and Input.is_action_pressed("jump"):
+		anim_player.play("Armature|Jump")
+	
+	if Input.is_action_just_released("move_right") or Input.is_action_just_released("move_left") or Input.is_action_just_released("move_forward") or Input.is_action_just_released("move_back"):
+		anim_player.play("Armature|Iddle")
+	
 	move_and_slide()
