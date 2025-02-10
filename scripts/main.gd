29327adcb47d3e7929d2c8417c5d95cc7b88714a
Nikola Talevski <talevskinikola1409@gmail.com>
2025-02-10 02:16:07 +0100

    Add timer

--- scripts/main.gd
+++ scripts/main.gd
@@ -6,11 +6,20 @@ extends Node
 @export var treasure_scene_4: PackedScene
 @export var treasure_scene_5: PackedScene
 
+@onready var timer = $Timer
+@onready var timer_label = $UserInterface/TimerLabel
+var time_left = 30
+
 var treasure_array = []
 
 func _ready() -> void:
 	randomize()
 	treasure_array = [treasure_scene_1, treasure_scene_2, treasure_scene_3, treasure_scene_4, treasure_scene_5]
+	
+	timer.wait_time = 1.0
+	timer.timeout.connect(_on_timer_timeout)
+	timer.start()
+	update_ui()
 
 func _on_treasure_collected() -> void:
 	var treasure_scene = treasure_array[randi() % treasure_array.size()]
@@ -23,3 +32,18 @@ func _on_treasure_collected() -> void:
 	
 	treasure.collected.connect($UserInterface/ScoreLabel._on_treasure_collected.bind())
 	treasure.collected.connect(_on_treasure_collected.bind())
+
+func _on_timer_timeout():
+	if time_left > 0:
+		time_left -= 1
+		update_ui()
+	
+		if time_left == 0:
+			game_over()
+
+func update_ui():
+	timer_label.text = str(time_left) + "s"
+
+func game_over():
+	timer.stop()
+	print("Game Over!")
